ff50a680ff24edb0ee74c9c02af0864d15ca6743
schweinsteiger159 <55477988+schweinsteiger159@users.noreply.github.com>
2020-05-21 22:09:24 +0700

    create new project

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
 import React from 'react';
 import './App.css';
-import RouterURL from './components/Router/routerURL';
-import Header from './components/header';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import Home from './components/pages/home';
-import ShowingMovie from './components/pages/showing-movie';
-import ComingMovie from './components/pages/coming-movie';
-import Detail from './components/pages/detail'
-import AdminFilm from './components/pages/AdminFilm';
-import BookTicket from './components/pages/book-ticket';
-import Footer from './components/footer';
-import Login from './components/pages/login';
+
 
 function App() {
 
   return (
-    <Router>
-      <Header></Header>
-      
-      <Route exact path="/" component={Home}></Route>
-      <Route path="/showing-movie" component={ShowingMovie}></Route>
-      <Route path="/coming-movie" component={ComingMovie}></Route>
-
-      <Route path="/detail/:id" component={Detail}></Route>
-      <Route path="/book-ticket" component={BookTicket}></Route>
-      <Route path="/customer/login" component={Login}></Route>
-
-      <Route path="/admin/film" component={AdminFilm}></Route>
-      
-      
-      <Footer></Footer>
-
-      
-    </Router>
+    <div>Hello world</div>
 
 
   );
